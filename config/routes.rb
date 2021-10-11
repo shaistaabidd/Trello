52a60bf5022ec486deba4f0018a705a22f64115a
shaistaabidd <shaistaabid76@gmail.com>
2021-10-11 18:34:24 +0500

    trello

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,16 @@
 Rails.application.routes.draw do
-  devise_for :users
+  #devise_for :users
   #get 'home/index'
+  #devise_for :users, path: 'auth', path_names: { sign_in: 'login', sign_out: 'logout', password: 'secret', confirmation: 'verification', unlock: 'unblock', registration: 'register', sign_up: 'cmon_let_me_in' }
+
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "home#index" 
+  devise_for :users, :controllers => {
+    :registrations => "users/registrations",
+    :sessions => "users/sessions",
+    :passwords => "users/passwords",
+    :confirmations => "users/confirmations",
+    :invitations => "users/invitations"
+
+  }
 end
